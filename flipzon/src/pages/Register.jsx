6dc75e2fe0e8b366d429224c77e7576fdaea90dd
BRUGUSOFT <kiran.paturi@brugu.io>
2023-12-11 15:08:37 +0530

    added code to fetch countries

--- flipzon/src/pages/Register.jsx
+++ flipzon/src/pages/Register.jsx
@@ -1,39 +1,54 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { DropDown } from "../components/DropDown"
 import { TextBox } from "../components/TextBox"
 import { registerConfig } from "../config/registerConfig"
+import axios from "axios"
 
-export const Register =({})=>{
-    const[register,setRegister] = useState({
-        FirstName:"",
-        LastName:"",
-        Country:"",
-        Password:"",
-        ConfirmPassword:""
+export const Register = ({ }) => {
+    const [register, setRegister] = useState({
+        FirstName: "",
+        LastName: "",
+        Country: "",
+        Password: "",
+        ConfirmPassword: ""
     });
-    const handleChange = (e)=>{
+    const [countryList,setCountryList] = useState([]);
+    const handleChange = (e) => {
         let data = register;
-        data[e.target.name]=e.target.value;
-        setRegister({...data});
+        data[e.target.name] = e.target.value;
+        setRegister({ ...data });
     };
-    const countryList =[{
-        text:"India",
-        value:"IN"
-    },
-{
-    text:"United States",
-    value:"USA"
-}]
-    return(
+
+
+    const getCountries = async()=>{
+        const url="https://restcountries.com/v2/all";
+        try{
+            let result = await axios.get(url);
+            
+            const mappedResult = result.data.map(x=>{
+                return {text:x.name,value:x.alpha3Code}
+            });
+            console.log(mappedResult);
+            setCountryList(mappedResult);
+        }catch(ex){
+            console.log(ex);
+        };
+    };
+    useEffect(()=>{
+        //at the time of loading the component.
+        getCountries();
+    },[]);
+
+    return (
         <form className="ml-5 mt-5">
             <TextBox config={registerConfig.FirstName}
-             onChange={handleChange} />
-            <TextBox config={registerConfig.LastName} onChange={handleChange}/>
-            <DropDown config={registerConfig.Country} 
-            data={countryList}
-            onChange={handleChange}/>
-            <TextBox config={registerConfig.Password} onChange={handleChange}/>
-            <TextBox config={registerConfig.ConfirmPassword} onChange={handleChange}/>
+                onChange={handleChange} />
+            <TextBox config={registerConfig.LastName} onChange={handleChange} />
+            <DropDown config={registerConfig.Country}
+                data={countryList}
+                onChange={handleChange} />
+            <TextBox config={registerConfig.Password} onChange={handleChange} />
+            <TextBox config={registerConfig.ConfirmPassword} onChange={handleChange} />
             <pre>{JSON.stringify(register)}</pre>
         </form>
     )
