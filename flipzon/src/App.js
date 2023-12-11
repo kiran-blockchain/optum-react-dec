c0ef7dba980bd5254b86b4c39267e00ce70099ea
BRUGUSOFT <kiran.paturi@brugu.io>
2023-12-11 10:58:03 +0530

    added code

---
+++ flipzon/src/App.js
@@ -0,0 +1,12 @@
+
+//components must start with Capital Letter
+//Component must return only one root tag 
+// All the html Tags must be closed.
+// use className instead of class while assigning the CSS
+export const App = ({})=>{
+  return(
+    <div>
+      <h1>Welcome to Flip zon</h1>
+    </div> 
+  )
+};
